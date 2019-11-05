704dcfbce8776fd9e8ee7746024c549b1dee3a0c
דניאל כהן <daniel7777cohen@gmail.com>
2019-11-05 21:58:13 +0200

    tryingSearchApi

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var unirest = require("unirest"); 
+var unirest = require("unirest");
 var cloudinary = require("cloudinary").v2;
 var express = require('express')
   , cors = require('cors')
@@ -7,22 +7,6 @@ const corsOptions = {
   origin: true,
   credentials: true
 };
-
-var readFile=function(req, res){
-  var fs = require('fs');
-  var returnData = {};
-
-  fs.readFile('C:\Users\danie\Downloads\Westworld.mp3', function(err, file){
-      var base64File = new Buffer(file, 'binary').toString('base64');
-console.log(file);
-      returnData.fileContent = base64File;
-
-      res.json(returnData);
-  });
-}
-
-
-
 
 //const multer = require('multer');
 var bodyParser = require("body-parser");
@@ -42,7 +26,7 @@ cloudinary.config({
   api_secret: '8ESQHQSTHNzYuuvwPxivCuzSwHU'
 });
 
-var allowCrossDomain = function(req, res, next) {
+var allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
@@ -51,10 +35,10 @@ var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
     return res.status(200).json({});
-};
+  };
   next();
 }
-String.prototype.splice = function(idx, rem, str) {
+String.prototype.splice = function (idx, rem, str) {
   return this.slice(0, idx) + str + this.slice(idx + Math.abs(rem));
 };
 
@@ -62,7 +46,47 @@ app.use(allowCrossDomain);
 
 app.options('*', cors(corsOptions)); // preflight OPTIONS; put before other routes
 
+
+
 app.get("/", function (req, res) {
+  
+  var fs = require('fs');
+
+
+  var LineByLineReader = require('line-by-line'),
+  lr = new LineByLineReader('facetokens.txt');
+
+lr.on('error', function (err) {
+// 'err' contains error object
+});
+
+//lr.on('line', function (line) {
+// pause emitting of lines...
+//lr.pause();
+
+
+
+/*
+if(res.faces&&res.faces[0])
+{
+  console.log(res.faces[0]);
+  fs.appendFileSync('facetokens.txt', res.faces[0].face_token + "\n");
+  lr.resume();
+
+     // ...and continue emitting lines.
+}else  lr.resume();
+});
+*/
+// ...do your asynchronous line processing..
+
+
+
+
+//});
+
+lr.on('end', function () {
+// All lines are read, file is closed now.
+});
 
   res.render("FaceApp.ejs");
 });
@@ -94,47 +118,66 @@ app.post('/submit', function (req, res) {
 
 app.post('/upload', function (req, res) {
   req.setTimeout(500000);
-  
 
-  
+
+
   console.log("inside post");
   console.log(req.body.your_data);
 
 
   if (req.body.your_data) {
 
-    //var secondrequestFromApi = unirest("POST", "https://api-us.faceplusplus.com/facepp/v1/face/thousandlandmark");
+    var facepp = require('face-plusplus-node');
+  facepp.setApiKey('5hMTiSrwjcK1QY-dlqbh94430XBwiiU0');
+  facepp.setApiSecret('gQiWxwY7RtJm8ALC8g_22wM5phvQxMVD');
+
+
     var requestFromApi = unirest("POST", "https://faceplusplus-faceplusplus.p.rapidapi.com/facepp/v3/detect");
 
     console.log("inside if req.body.your_data");
-    console.log("original url is " +req.body.your_data)
+    console.log("original url is " + req.body.your_data)
     var MAX_WIDTH = 1000;
     var MAX_HEIGHT = 1000;
     var width = (req.body.width);
     var height = (req.body.height);
 
     if (width > height) {
-        if (width > MAX_WIDTH) {
-            height *= MAX_WIDTH / width;
-            width = MAX_WIDTH;
-        }
+      if (width > MAX_WIDTH) {
+        height *= MAX_WIDTH / width;
+        width = MAX_WIDTH;
+      }
     } else {
-        if (height > MAX_HEIGHT) {
-            width *= MAX_HEIGHT / height;
-            height = MAX_HEIGHT;
-        }
+      if (height > MAX_HEIGHT) {
+        width *= MAX_HEIGHT / height;
+        height = MAX_HEIGHT;
+      }
     }
 
     let url_ = req.body.your_data;
     var n = url_.indexOf("upload");
-    var resizeParameters = "/w_"+Math.round(width)+",h_"+Math.round(height)+",c_scale"
-   url_ = url_.splice(n+6, 0, resizeParameters);//n + upload
-   console.log("new url_ = " + url_);
+    var resizeParameters = "/w_" + Math.round(width) + ",h_" + Math.round(height) + ",c_scale"
+    url_ = url_.splice(n + 6, 0, resizeParameters);//n + upload
+    console.log("new url_ = " + url_);
+
+//for Search Api
+    var parameters = {
+      image_url: url_,
+      faceset_token : '1c0d38a898eef4ce12d01e0aced20602',
+   
+    };
+    facepp.post('/search', parameters, function (err, res) {
+      if(err)
+      {
+        console.log(err);
+      }
+      else {console.log(res);
+      }
     
+    });
 
     requestFromApi.query({
       return_attributes: "gender,age,smiling,facequality,eyestatus,emotion,ethnicity,beauty,skinstatus",
-      return_landmark : "2",
+      return_landmark: "2",
       image_url: url_
     });
 
@@ -157,32 +200,32 @@ app.post('/upload', function (req, res) {
           console.log(element);
         });
 
-          global.detailsFromApi = detailsFromApi_;
-         global.url = url_;
-/*
-          secondrequestFromApi.query({
-            api_key : "5hMTiSrwjcK1QY-dlqbh94430XBwiiU0	",
-            api_secret : "gQiWxwY7RtJm8ALC8g_22wM5phvQxMVD",
-            return_landmark: "1",
-            image_url : global.url
-          });
-
-          secondrequestFromApi.end(function(result){
-            if (result.error) console.log(result.error);
-
-else console.log(result);console.log("inside second request end");
-
-          });
-*/
-
-          
-          res.end();
-        }
-        });
-
+        global.detailsFromApi = detailsFromApi_;
+        global.url = url_;
+        /*
+                  secondrequestFromApi.query({
+                    api_key : "5hMTiSrwjcK1QY-dlqbh94430XBwiiU0	",
+                    api_secret : "gQiWxwY7RtJm8ALC8g_22wM5phvQxMVD",
+                    return_landmark: "1",
+                    image_url : global.url
+                  });
         
-      
-    
+                  secondrequestFromApi.end(function(result){
+                    if (result.error) console.log(result.error);
+        
+        else console.log(result);console.log("inside second request end");
+        
+                  });
+        */
+
+
+        res.end();
+      }
+    });
+
+
+
+
   } else alert(" Try a different photo or tell Daniel about it :)");
 });
 
@@ -192,8 +235,8 @@ else console.log(result);console.log("inside second request end");
 
 
 
-var server = app.listen(port, function(){
+var server = app.listen(port, function () {
   console.log('CORS-enabled web server listening on port 8080');
-   
+
 });
 server.setTimeout(500000);
